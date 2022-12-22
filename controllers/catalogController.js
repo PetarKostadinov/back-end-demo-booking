63171d8ab48f21d4b163adede7d1efb5231792a1
PetarKostadinov <petarkostadinov30@gmail.com>
2022-12-22 21:20:31 +0200

    Added CSS; implemented Catalog and Details

---
+++ controllers/catalogController.js
@@ -0,0 +1,32 @@
+const { getAll, getById } = require('../services/accomodationService');
+
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    const rooms = getAll();
+
+    res.render('catalog', {
+        title: 'All Accomodation',
+        rooms
+    });
+});
+
+router.get('/:id', (req, res) => {
+    const roomId = req.params.id;
+    const room = getById(roomId);
+
+    if(room){
+
+        res.render('details', {
+            title: 'Accomodation Details',
+            room
+        });
+    }else{
+        res.render('roomNotFound', {
+            title: 'Accomodation Details',
+            roomId
+        });
+    }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const hbs = require('express-handlebars').create({
     extname: '.hbs'
 });
 
+const homeController = require('./controllers/homeController');
+const catalogController = require('./controllers/catalogController');
+const createController = require('./controllers/createController');
+const defaultController = require('./controllers/defaultController');
+const defaultTitle = require('./middlewares/defaultTitle');
 const app = express();
+
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 
 app.use(express.urlencoded({extended: true}));
 app.use('/static', express.static('static'));
+
+app.use(defaultTitle('Accomodation'))
+
+app.use(homeController);
+app.use('/catalog', catalogController);
+app.use('/create', createController);
+
+app.all('*', defaultController);
 
 app.listen(3000, () => console.log('listening on port 3000'));
