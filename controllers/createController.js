a5ef4495f7fd5b999c31afb008c4d65af1b451e3
PetarKostadinov <petarkostadinov30@gmail.com>
2022-12-23 23:13:08 +0200

    New CSS implementation

--- controllers/createController.js
+++ controllers/createController.js
@@ -1,9 +1,24 @@
+const { create } = require('../services/accomodationService');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('create', {
         title: 'Host New Accomodation'
     });
+});
+
+router.post('/', async (req, res) => {
+    try {
+       const result = await create(req.body);
+        res.redirect('/catalog/' + result.id);
+    } catch (err ) {
+        res.render('create', {
+            title: 'Request Error',
+            error: err.message.split('\n')
+        });
+    }
+
 });
 
 
--- services/accomodationService.js
+++ services/accomodationService.js
@@ -24,7 +24,36 @@ function getById(id) {
     return data.find(i => i.id == id);
 }
 
+async function create(roomData) {
+    const room = {
+        id: getId(), 
+        name: roomData.name,
+        description: roomData.description,
+        city: roomData.city,
+        beds: Number(roomData.beds),
+        price: Number(roomData.price),
+        imgUrl: roomData.imgUrl
+
+    };
+
+    const missing = Object.entries(room).filter(([k,v]) => !v);
+
+    if(missing.length > 0) {
+        throw new Error(missing.map(m => `${m[0]} is required!`).join('\n'));
+    }
+
+    data.push(room);
+    await persist();
+
+    return room;
+}
+
+function getId() {
+    return ('000000' + (Math.random() * 999999 | 0).toString(16)).slice(-6);
+}
+
 module.exports = {
     getAll,
-    getById
+    getById,
+    create
 }
